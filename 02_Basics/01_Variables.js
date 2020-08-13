e512d846cd9c71bb7d9b2c6b4f4b18ce1346bda4
Rohan Khurana <rohan20k@uw.edu>
2020-08-12 21:59:19 -0700

    First Commit

---
+++ 02_Basics/01_Variables.js
@@ -0,0 +1,23 @@
+var fullName = "Rohan Khurana";
+
+// How to name Variables
+// fullName This is called as case
+// full_name This is called snake case
+// full-name
+// FullName - Not advised to be used
+
+var courseName = "ReactJS Bootcamp";
+
+var isLoggedIn = true;
+
+
+// How many times users are logging in - 
+var loggedCount = 34;
+
+// Declaring variable with payment but with no value specified
+
+var paymentMode;
+
+console.log(paymentMode)
+
+paymentMode = "Credit Card";
---
+++ 02_Basics/02_Variables-two.js
@@ -0,0 +1,19 @@
+const uid = "abc123";
+var fullName = "Rohan Khurana";
+var email = "rohan20k@uw.edu";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false; 
+
+console.log(uid);
+console.log("Full Name is: "+ fullName);
+console.log(email);
+
+console.log(`
+    With unique id as: ${uid}
+    User name is: ${fullName}
+    User email is: ${email}
+    User password is: ${password}
+`)
+
