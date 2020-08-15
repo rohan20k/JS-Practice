7730dfa2e57c509f3e4b21b2be5a89128c69dfae
Rohan Khurana <rohan20k@uw.edu>
2020-08-14 17:14:52 -0700

    FilterArray and Array completed

--- 03_Intermediate/08arrayPartTwo.js
+++ 03_Intermediate/08arrayPartTwo.js
@@ -1,10 +1,34 @@
-function isEven(element){
-    if (element % 2 === 0){
-        return true;
-    } else{
-        return false;
+// function isEven(element){
+//     if (element % 2 === 0){
+//         return true;
+//     } else{
+//         return false;
         
-    }
-}
+//     }
+// }
 
-console.log(isEven(3));
+// function isEven(element){
+//     return element % 2 === 0; // This is the better approach to write the code as it is more efficient.
+// }
+// console.log(isEven(3));
+
+// var isEven = (element) => {
+//    return element % 2 === 0; 
+// }
+
+// // console.log(isEven(3));
+
+// var result = [2,4,14,6,22,8].every()//Every goes into the array and checks and the run the test for every element of the array. 
+// console.log(result);
+
+// We can write a call back 
+
+// var result = [2, 3, 6, 8].every((e) => {
+//     return e % 2 === 0;
+// });
+
+var result = [2, 4, 6, 8].every((e) => (e% 2 ===0));
+console.log(result);
+
+
+
---
+++ 03_Intermediate/09_filterArray.js
@@ -0,0 +1,41 @@
+var testArray = [2,4,6,3,1,5,9,8];
+
+// console.log(testArray.fill(0)); // All the values are replaced by 0; 
+
+// console.log(testArray.fill("R")); // All values are replaced by "R";
+
+// console.log(testArray.fill("O",2)) // The first argument is what value, the second argument till what value do I need to run. 
+
+// .fill(value,start,end) both the start is inclusive, end is not. 
+
+// console.log(testArray.fill("p",0,3));
+// ------------------------------------------------------------------------------------------------
+
+// const myNumber = [12,34,54,23,22,55,175,200,300];
+
+// const result = myNumber.filter(num=>num != 55);
+
+// console.log(result)
+
+// const result_2 = myNumber.filter(num => num < 55);
+
+// console.log(result_2)
+
+
+// ------------------------------------------- Splicing and Slicing ------------------------------------
+
+var users = ["Ram","Siya","Ram","Jai","Jay","Rom"];
+
+// console.log(users.slice(1));
+
+// users.splice(2,2,"Hi"); //For splice, the first value is start, second is the count from the first index and instead of them replace the value with the third argument. 
+// console.log(users);
+
+users.splice()
+console.log(users);
+
+
+
+
+
+
